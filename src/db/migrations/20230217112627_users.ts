0050d89029c550d7c3d49fd2e5d0c3ede10f12b8
Jeff Keith <me@jeffkeith.me>
2023-02-27 08:54:06 -0500

    init

--- src/db/migrations/20230217112627_users.ts
+++ src/db/migrations/20230217112627_users.ts
@@ -1,19 +1,19 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable("authed.users", function (table) {
+  return knex.schema.createTable("users", function (table) {
     table.uuid("id").primary().unique().defaultTo(knex.raw('gen_random_uuid()'));
     table.uuid("auth_id").unique().notNullable();
     table.string("email").notNullable();
   })
-  .createTable("authed.profile", function (table) {
+  .createTable("profile", function (table) {
     table.uuid("id").primary().unique().defaultTo(knex.raw('gen_random_uuid()'));
     table.string("first_name").notNullable();
     table.string("last_name").notNullable();
-    table.uuid('user_id').references('id').inTable('authed.users').notNullable();
+    table.uuid('user_id').references('id').inTable('users').notNullable();
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable("authed.profile").dropTable("authed.users");
+  return knex.schema.dropTable("authed.profile").dropTable("users");
 }
